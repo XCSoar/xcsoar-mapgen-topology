c05e2d11e7ab7f6018c0a228f6b00ad106cad99a
Philipp Wollschlegel <folken@kabelsalat.ch>
2023-05-31 18:27:35 +0200

    use config parser for scripts and central configuration

--- reducecity.py
+++ reducecity.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 
 import psycopg2
+from configparser import ConfigParser
+
+# Read the connection information from the configuration file
+config = ConfigParser()
+config.read('config.ini')
+database = config.get('postgresql', 'database')
+user = config.get('postgresql', 'user')
+password = config.get('postgresql', 'password')
+host = config.get('postgresql', 'host')
+port = config.get('postgresql', 'port')
 
 # Connect to the PostGIS database with OpenStreetMap data imported using osm2pgsql
-conn = psycopg2.connect(
-    "dbname=osm user=osmuser password=newgis23 host=worldbox port=5432"
-)
+conn = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
 cur = conn.cursor()
 
 # Create a table for the filtered and simplified polygons
