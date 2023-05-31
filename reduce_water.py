155eceb6742b2d4b2b1f1b62e8d4b365b66fca20
Philipp Wollschlegel <folken@kabelsalat.ch>
2023-05-31 18:59:18 +0200

    reformat files to current python standard

--- reduce_water.py
+++ reduce_water.py
@@ -9,17 +9,17 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 
 # Create a table for the filtered and simplified polygons
-cur.execute("""
+cur.execute(
+    """
     DROP TABLE IF EXISTS water_polygons;
     CREATE TABLE water_polygons AS
     SELECT osm_id, ST_SimplifyPreserveTopology(way, 15) AS way
     FROM planet_osm_polygon
     WHERE ("natural" = 'water' OR "waterway" = '*' OR "landuse" = 'reservoir') AND ST_Area(way) >= 10000;
-""")
+"""
+)
 conn.commit()
 
 # Close the database connection
 cur.close()
 conn.close()
-
-
