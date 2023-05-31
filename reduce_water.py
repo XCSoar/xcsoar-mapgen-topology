a705d8f607669048442c0c883879516fce46fe9b
Philipp Wollschlegel <folken@kabelsalat.ch>
2023-05-31 19:03:54 +0200

    reduce_water: rename and connection via config

--- reduce_water.py
+++ reduce_water.py
@@ -1,24 +1,55 @@
 #!/usr/bin/python3
 
 import psycopg2
+from configparser import ConfigParser
 
-# Connect to the PostGIS database with OpenStreetMap data imported using osm2pgsql
+# Read the connection information from the configuration file
+config = ConfigParser()
+config.read("config.ini")
+database = config.get("postgresql", "database")
+user = config.get("postgresql", "user")
+password = config.get("postgresql", "password")
+host = config.get("postgresql", "host")
+port = config.get("postgresql", "port")
+
+# Connect to the PostgreSQL database
 conn = psycopg2.connect(
-    "dbname=osm user=osmuser password=newgis23 host=localhost port=5432"
+    database=database, user=user, password=password, host=host, port=port
 )
 cur = conn.cursor()
 
 # Create a table for the filtered and simplified polygons
 cur.execute(
     """
-    DROP TABLE IF EXISTS water_polygons;
-    CREATE TABLE water_polygons AS
-    SELECT osm_id, ST_SimplifyPreserveTopology(way, 15) AS way
-    FROM planet_osm_polygon
-    WHERE ("natural" = 'water' OR "waterway" = '*' OR "landuse" = 'reservoir') AND ST_Area(way) >= 10000;
+DROP TABLE IF EXISTS water_polygons_small;
+DROP TABLE IF EXISTS water_polygons_large;
+
+CREATE TABLE water_polygons_small AS
+SELECT osm_id, name, ST_SimplifyPreserveTopology(way, 15) AS way
+FROM planet_osm_polygon
+WHERE ("natural" = 'water' OR "landuse" = 'reservoir' OR "landuse" = 'basin')
+  AND ST_Area(way) >= 1000 AND ST_Area(way) < 300000;
+
+CREATE TABLE water_polygons_large AS
+SELECT osm_id, name, ST_SimplifyPreserveTopology(way, 15) AS way
+FROM planet_osm_polygon
+WHERE ("natural" = 'water' OR "landuse" = 'reservoir' OR "landuse" = 'basin')
+  AND ST_Area(way) >= 300000;
 """
 )
 conn.commit()
+
+cur.execute(
+    """
+DROP TABLE IF EXISTS water_lines;
+CREATE TABLE water_lines AS
+SELECT osm_id, name, ST_Simplify(way, 15) AS way
+FROM planet_osm_line
+WHERE waterway = 'river';
+"""
+)
+conn.commit()
+
 
 # Close the database connection
 cur.close()
