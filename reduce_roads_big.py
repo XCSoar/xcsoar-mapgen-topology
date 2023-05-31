5e8246f6f8ff34e4d9d889d48bcd5c6d851be427
Philipp Wollschlegel <folken@kabelsalat.ch>
2023-05-31 18:58:52 +0200

    parse connection details from config.ini

---
+++ reduce_roads_big.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import psycopg2
+from configparser import ConfigParser
+
+# Read the connection information from the configuration file
+config = ConfigParser()
+config.read('config.ini')
+database = config.get('postgresql', 'database')
+user = config.get('postgresql', 'user')
+password = config.get('postgresql', 'password')
+host = config.get('postgresql', 'host')
+port = config.get('postgresql', 'port')
+
+# Connect to the PostgreSQL database
+conn = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+cur = conn.cursor()
+
+# Create a table for the filtered and simplified polygons
+cur.execute("""
+    DROP TABLE IF EXISTS reduced_roads_big;
+    CREATE TABLE reduced_roads_big AS
+    SELECT osm_id, ST_Simplify(way, 30) AS way_reduced
+    FROM planet_osm_roads
+    WHERE ("highway" = 'motorway' OR "highway" = 'primary' OR "highway" = 'primary_link')
+""")
+conn.commit()
+
+# Close the database connection
+cur.close()
+conn.close()
---
+++ reduce_roads_small.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import psycopg2
+from configparser import ConfigParser
+
+# Read the connection information from the configuration file
+config = ConfigParser()
+config.read('config.ini')
+database = config.get('postgresql', 'database')
+user = config.get('postgresql', 'user')
+password = config.get('postgresql', 'password')
+host = config.get('postgresql', 'host')
+port = config.get('postgresql', 'port')
+
+# Connect to the PostgreSQL database
+conn = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+cur = conn.cursor()
+
+# Create a table for the filtered and simplified polygons
+cur.execute("""
+    DROP TABLE IF EXISTS reduced_roads_small;
+    CREATE TABLE reduced_roads_small AS
+    SELECT osm_id, ST_Simplify(way, 30) AS way_reduced
+    FROM planet_osm_roads
+    WHERE "highway" = 'residential' or "highway" = 'unclassified' or "highway" = 'tertiary'
+""")
+conn.commit()
+
+# Close the database connection
+cur.close()
+conn.close()
+
+
