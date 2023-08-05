2a7c6127bff83fe331e0362937cecc824c59b799
Philipp Wollschlegel <folken@kabelsalat.ch>
2023-08-06 01:46:44 +0200

    airstrips: crs assignment and export

--- scripts/export_airstrips.py
+++ scripts/export_airstrips.py
@@ -19,34 +19,26 @@ conn = psycopg2.connect(
 )
 
 # Set the SQL queries to retrieve the desired geometries
-sql_small = "SELECT * FROM water_polygons_small"
-sql_large = "SELECT * FROM water_polygons_large"
-sql_lines = "SELECT * FROM water_lines"
+sql_small = "SELECT * FROM runway_polygons"
 
 # Fetch the geometries from the database and create GeoDataFrames
-gdf_small = gpd.GeoDataFrame.from_postgis(sql_small, conn, geom_col="way")
-gdf_large = gpd.GeoDataFrame.from_postgis(sql_large, conn, geom_col="way")
-gdf_lines = gpd.GeoDataFrame.from_postgis(sql_lines, conn, geom_col="way")
+gdf_small = gpd.GeoDataFrame.from_postgis(sql_small, conn, geom_col="polygon")
 
 # Set the output shapefile paths
 output_dir = "out/"
-output_shapefile_small = output_dir + "water_area_small.shp"
-output_shapefile_large = output_dir + "water_area_large.shp"
-output_shapefile_lines = output_dir + "water_lines.shp"
+output_shapefile_small = output_dir + "airstrip_area.shp"
+
+gdf_small = gdf_small.set_crs("EPSG:3857")
 
 # Set the CRS (WGS84)
-crs = "EPSG:4326"
+gdf_small = gdf_small.to_crs("EPSG:4326")
 
 # Export the GeoDataFrames to shapefiles with ISO encoding and WGS84 SRS
 gdf_small.to_file(
-    output_shapefile_small, driver="ESRI Shapefile", encoding="ISO-8859-1", crs=crs
+    output_shapefile_small,
+    driver="ESRI Shapefile",
+    encoding="ISO-8859-1",
+    crs="EPSG:4326",
 )
-gdf_large.to_file(
-    output_shapefile_large, driver="ESRI Shapefile", encoding="ISO-8859-1", crs=crs
-)
-gdf_lines.to_file(
-    output_shapefile_lines, driver="ESRI Shapefile", encoding="ISO-8859-1", crs=crs
-)
-
 # Close the database connection
 conn.close()
--- scripts/reduce_airstrips_lines.py
+++ scripts/reduce_airstrips_lines.py
@@ -20,39 +20,34 @@ cur = conn.cursor()
 
 # Create a table for the filtered and simplified polygons
 cur.execute(
-        """
+    """
 -- Drop the table if it already exists
 DROP TABLE IF EXISTS runway_polygons;
 
 -- Create a new table to store the rectangular polygons
-CREATE TABLE runway_polygons AS (
-    SELECT
-        osm_id,
-        aeroway,
-        ref,
-        CASE
-            WHEN width ~ '^[-+]?[0-9]+(\.[0-9]+)?$' THEN width::numeric
-            ELSE 30
-        END AS width,
-        ST_Buffer(ST_MakeValid(way), CASE WHEN width ~ '^[-+]?[0-9]+(\.[0-9]+)?$' THEN width::numeric / 2 ELSE 15 END, 'endcap=flat join=mitre') AS polygon
-    FROM
-        planet_osm_line
-    WHERE
-        aeroway = 'runway' AND ST_IsValid(way)
+CREATE TABLE runway_polygons (
+    osm_id bigint,
+    aeroway varchar(255),
+    ref varchar(255),
+    width numeric,
+    polygon geometry(Polygon, 3857) -- Set SRID to 4326 (WGS84)
 );
 
--- Add the surface column to the runway_polygons table
-ALTER TABLE runway_polygons ADD COLUMN surface text;
-
--- Insert the surface attribute from planet_osm_polygon into the runway_polygons table
-UPDATE runway_polygons
-SET surface = p.surface
-FROM (
-    SELECT osm_id, surface, ref
-    FROM planet_osm_polygon
-    WHERE aeroway = 'runway'
-) AS p
-WHERE runway_polygons.osm_id = p.osm_id;
+-- Insert data into the new table with explicit SRID setting
+INSERT INTO runway_polygons (osm_id, aeroway, ref, width, polygon)
+SELECT
+    osm_id,
+    aeroway,
+    ref,
+    CASE
+        WHEN width ~ '^[-+]?[0-9]+(\.[0-9]+)?$' THEN width::numeric
+        ELSE 30
+    END AS width,
+    ST_SetSRID(ST_Buffer(ST_MakeValid(way), CASE WHEN width ~ '^[-+]?[0-9]+(\.[0-9]+)?$' THEN width::numeric / 2 ELSE 15 END, 'endcap=flat join=mitre'), 3857) AS polygon
+FROM
+    planet_osm_line
+WHERE
+    aeroway = 'runway' AND ST_IsValid(way);
 
 -- Add an index for better query performance
 CREATE INDEX idx_runway_polygons ON runway_polygons USING GIST (polygon);
