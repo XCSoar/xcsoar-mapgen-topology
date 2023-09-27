5c50dd7d4d8da4854ce071a87c58355cbd3f7e87
folken <folken@worldbox.apwm.home>
2023-09-27 23:11:46 +0200

    redu

---
+++ scripts/reduce_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./reduce_railway.py
+./reduce_power.py
+./reduce_water.py
+./reduce_airstrips_lines.py
+./reduce_city.py
+./reduce_forest.py
+./reduce_roads_big.py
+./reduce_roads_medium.py
+./reduce_roads_small.py
