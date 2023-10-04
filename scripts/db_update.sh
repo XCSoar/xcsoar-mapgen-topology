712bda23d87e572c9e5518d681c978ab4253f062
Philipp Wollschlegel <folken@kabelsalat.ch>
2023-10-04 17:56:48 +0200

    db update for torrent

--- scripts/db_update.sh
+++ scripts/db_update.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 osm2pgsql -C 14000 --slim -v -U osmuser -W -d osm -S ../conf/default.style -H localhost -a "$1"
+osm2pgsql-replicate init -H aria2c --seed-time=0 https://planet.openstreetmap.org/pbf/planet-latest.osm.pbf.torrent
